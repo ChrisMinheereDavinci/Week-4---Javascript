2c73c6e832ededcde8fa86c677ea9f66781332db
Chris_Minheere <99040522@mydavinci.nl>
2018-09-19 12:32:22 +0200

    Debugging via console and delete wrong js file

--- Lab 1 - Kaas/js/main.js
+++ Lab 1 - Kaas/js/main.js
@@ -1,10 +1,17 @@
-var KaasGeel = prompt("Is de kaas geel?");
+alert("VRAGEN BEANDWOORDEN MET ja OF nee");
+
+var gelekaas = prompt("Is de kaas geel?");
 var gaten
 var blauweschimmel
 
-if(KaasGeel == 'ja'){
+
+
+if(gelekaas == 'ja'){
+    console.log('geel dus he! zeg je');
     gaten = prompt("Zitten er gaten in?");
-} else if(KaasGeel == 'nee'){
+
+} else if(gelekaas == 'nee'){
+    console.log('niet geel? Welke kleur dan wel?');
     blauweschimmel = prompt("heeft de kaas blauwe schimmels?");
 }
 
@@ -12,17 +19,22 @@ var duur
 var steen
 
 if(gaten == 'ja'){
-    duur = prompt("Is de kaas belachelijk duur?")
+    console.log('met gaten dus! MMmmm');
+    duur = prompt("Is de kaas belachelijk duur?");
+    
 } else if(gaten == 'nee'){
-    steen = prompt("Is de kaas hard als steen?")
+    console.log('Geen gaten dus!');
+    steen = prompt("Is de kaas hard als steen?");
 }
 
 var KorstJa
 var KorstNee
 
 if(blauweschimmel == 'ja'){
+    console.log('blauw? wat zeg je me nouw');
     KorstJa = prompt("Heeft de kaas een Korst?");
 } else if(blauweschimmel == 'nee'){
+    console.log('niet blauw? dat is raar!');
     KorstNee = prompt("Heeft de kaas een Korst?");
 }
 
@@ -30,34 +42,42 @@ var Camembert
 var Mozzarella
 
 if(KorstNee == 'ja'){
-    Camembert = document.write("Uw keuze is de Camembert kaas.")
+    console.log('met een korst, aparte keuze');
+    Camembert = document.write("Uw keuze is de Camembert kaas.");
 } else if(KorstNee == 'nee'){
-    Mozzarella = document.write("Uw keuze is de Mozzarella kaas.")
+    console.log('ik vind die korst ook vies hoor');
+    Mozzarella = document.write("Uw keuze is de Mozzarella kaas.");
 }
 
 var Rochbaron
 var Foume
 
 if(KorstJa == "ja"){
-    Rochbaron = document.write("Uw keuze is de Bleu de Rochbaron kaas.")
+    console.log('lekker zn korst');
+    Rochbaron = document.write("Uw keuze is de Bleu de Rochbaron kaas.");
 } else if(KorstJa == "nee") {
-    Foume = document.write("Uw keuze is de Foume d'Ambert kaas.")
+    console.log('ik vind die korst ook vies hoor');
+    Foume = document.write("Uw keuze is de Foume d'Ambert kaas.");
 }
 
 var emmenthaler
 var leerdammer
 
 if(duur == 'ja'){
-    emmenthaler = document.write("Uw keuze is de Emmenthaler kaas.")
+    console.log('dat is duur zeg');
+    emmenthaler = document.write("Uw keuze is de Emmenthaler kaas.");
 } else if(duur == 'nee') {
-    leerdammer = document.write("Uw keuze is de Leerdammer kaas.")
+    console.log('ik zou ook geen duure kaas willen hoor');
+    leerdammer = document.write("Uw keuze is de Leerdammer kaas.");
 }
 
 var pammigiano
 var goudsekaas
 
 if(steen == 'ja'){
-    pammigiano = document.write("Uw keuze is de Pammigiano Reggiano kaas.")
+    console.log('lekker zn harde kaas');
+    pammigiano = document.write("Uw keuze is de Pammigiano Reggiano kaas.");
 } else if(steen == 'nee') {
-    goudsekaas = document.write("Uw keuze is de Goudse kaas.")
+    console.log('ik vind zn harde kaas ook vies');
+    goudsekaas = document.write("Uw keuze is de Goudse kaas.");
 }
