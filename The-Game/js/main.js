47b8b44f1e453a2467b213d19577af5a03727e31
Chris_Minheere <99040522@mydavinci.nl>
2018-09-20 12:06:50 +0200

    index en js file added

---
+++ The-Game/js/main.js
@@ -0,0 +1,39 @@
+var score;
+score = 0;
+alert ('Haal 8 punten om de quiz te winnen')
+
+var vraag1
+vraag1 = prompt('1. Wat is het tegenoverstelde van conservatief?')
+if(vraag1 == "Progressief"){
+    score++;
+    alert('Correct! je score is nu: '+score)
+}else{
+    alert('Fout! je score is nu: '-score)
+}
+
+var vraag2
+vraag2 = prompt('2. In welk lichaamsdeel vind je het aambeeld?')
+if(vraag2 == "In het oor"){
+    score++;
+    alert('Correct! je score is nu: '+score)
+}else{
+    alert('Fout! je score is nu: '-score)
+}
+
+var vraag3
+vraag3 = prompt('3. Welke Amerikaanse krant heeft als bijnaam The Old Gray Lady?')
+if(vraag3 == "The New York Times"){
+    score++;
+    alert('Correct! je score is nu: '+score)
+}else{
+    alert('Fout! je score is nu: '-score)
+}
+
+var vraag4
+vraag4 = prompt('4. Hoe noem je een trap die spiraalvormig omhoog gaat?')
+if(vraag4 == "Een wenteltrap"){
+    score++;
+    alert('Correct! je score is nu: '+score)
+}else{
+    alert('Fout! je score is nu: '-score)
+}
